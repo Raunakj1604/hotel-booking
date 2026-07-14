92e6d1852edb7410798d2c162376bf62730d044d
Raunakj1604 <raunakj1604@gmail.com>
2026-07-14 14:48:47 +0530

    fixing  clerkroute

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware());
 
-app.use("/api/clerk",clerkWebhooks())
+app.use("/api/clerk",clerkWebhooks)
 
 app.get("/", (req,res)=>res.send("API is working fantastically"))
 
